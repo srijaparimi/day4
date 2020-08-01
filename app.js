5510388f7e7a02b9aedb93f51fcf5451ae26520a
srijaparimi <srijaparimi@gmail.com>
2020-08-01 11:32:44 +0530

    app.js updated

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 var count = 0;
+var btn1 = document.getElementById('btn');
+console.log(btn1)
+btn1.addEventListener('click', grater)
 
 function grater() {
     var number1 = document.getElementById('num1').value;
     var color = document.getElementById('color').value;
-    number1 = Number(number1);
-    for (let i = 0; i < number1; i++) {
+    for (var i = 0; i < number1; i++) {
         count++;
         var btn = document.createElement("button");
         btn.className = "btn1";
         btn.innerHTML = "new Button " + count;
-        btn.style = "background-color:" + (color) + ";"
+        btn.style = "background-color:" + (color) + ";";
         document.body.appendChild(btn);
-    }​
+    }
 }
